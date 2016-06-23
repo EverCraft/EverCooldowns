572420a3a9afeb5954174ad77e8b46c82f9ebeaa
rexbut <butbutdu45@gmail.com>
2016-06-24 00:10:52 +0200

    Update
    Signed-off-by: rexbut <butbutdu45@gmail.com>

--- build/tmp/updateLicenseMain/original/fr/evercraft/evercooldowns/ECConfig.java
+++ build/tmp/updateLicenseMain/original/fr/evercraft/evercooldowns/ECConfig.java
@@ -1,18 +1,18 @@
 /*
- * This file is part of EverCooldowns.
+ * This file is part of EverMails.
  *
- * EverCooldowns is free software: you can redistribute it and/or modify
+ * EverMails is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * EverCooldowns is distributed in the hope that it will be useful,
+ * EverMails is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with EverCooldowns.  If not, see <http://www.gnu.org/licenses/>.
+ * along with EverMails.  If not, see <http://www.gnu.org/licenses/>.
  */
 package fr.evercraft.evercooldowns;
 
