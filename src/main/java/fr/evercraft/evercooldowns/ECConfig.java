ace7c20936504557f48f82a44fad89b241419622
rexbut <butbutdu45@gmail.com>
2016-07-18 19:15:32 +0200

    Update Database
    Signed-off-by: rexbut <butbutdu45@gmail.com>

--- src/main/java/fr/evercraft/evercooldowns/ECConfig.java
+++ src/main/java/fr/evercraft/evercooldowns/ECConfig.java
@@ -34,5 +34,14 @@ public class ECConfig extends EConfig {
 	public void loadDefault() {
 		addDefault("debug", false, "Displays plugin performance in the logs");
 		addDefault("language", EMessage.FRENCH, "Select language messages", "Examples : ", "  French : FR_fr", "  English : EN_en");
+		
+		// SQL
+		addComment("SQL", 				"Save the user in a database : ",
+										" H2 : \"jdbc:h2:" + this.plugin.getPath().toAbsolutePath() + "/data\"",
+										" SQL : \"jdbc:mysql://[login[:password]@]<host>:<port>/<database>\"",
+										" Default users are saving in the 'data.mv.db'");
+		addDefault("SQL.enable", false);
+		addDefault("SQL.url", "jdbc:mysql://root:password@localhost:3306/minecraft");
+		addDefault("SQL.prefix", "evercooldowns_");
 	}
 }
