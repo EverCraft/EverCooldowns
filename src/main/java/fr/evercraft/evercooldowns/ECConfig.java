e98605fc29d23d6b969f2dd2c662271a3df9fe11
rexbut <butbutdu45@gmail.com>
2016-10-18 16:44:09 +0200

    Update : Config

--- src/main/java/fr/evercraft/evercooldowns/ECConfig.java
+++ src/main/java/fr/evercraft/evercooldowns/ECConfig.java
@@ -32,8 +32,8 @@ public class ECConfig extends EConfig {
 	
 	@Override
 	public void loadDefault() {
-		addDefault("debug", false, "Displays plugin performance in the logs");
-		addDefault("language", EMessage.FRENCH, "Select language messages", "Examples : ", "  French : FR_fr", "  English : EN_en");
+		addDefault("DEBUG", false, "Displays plugin performance in the logs");
+		addDefault("LANGUAGE", EMessage.FRENCH, "Select language messages", "Examples : ", "  French : FR_fr", "  English : EN_en");
 		
 		// SQL
 		addComment("SQL", 				"Save the user in a database : ",
