aeff179e10c7fb5587ff1a33d4ea1eec42a01566
rexbut <butbutdu45@gmail.com>
2017-09-17 21:29:46 +0200

    Update : Message

--- src/main/java/fr/evercraft/evercooldowns/ECMessage.java
+++ src/main/java/fr/evercraft/evercooldowns/ECMessage.java
@@ -31,51 +31,51 @@ public class ECMessage extends EMessage<EverCooldowns> {
 	}
 	
 	public enum ECMessages implements EnumMessage {
-		PREFIX("PREFIX", 									"[&4Ever&6&lCooldowns&f] "),
-		DESCRIPTION("description", 							"Gestion des cooldowns"),
-		COOLDOWN("cooldown", 								"&cVous ne pouvez pas effectuer cette action avant : {time}"),
+		PREFIX(								"[&4Ever&6&lCooldowns&f] "),
+		DESCRIPTION(						"Gestion des cooldowns"),
+		COOLDOWN(							"&cVous ne pouvez pas effectuer cette action avant : {time}"),
 		
 		// Clear
-		CLEAR_DESCRIPTION("clearDescription", 				"Supprime tous les cooldowns d'un joueur"),
-		CLEAR_STAFF("clearStaff", 							"&7Tous les cooldowns de &6{player} &7ont bien été supprimés."),
-		CLEAR_PLAYER("clearPlayer", 						"&7Tous vos cooldowns ont été supprimés par &6{staff}&7."),
-		CLEAR_EQUALS("clearEquals", 						"&7Tous vos cooldowns ont bien été supprimés."),
-		CLEAR_ALL("clearAll", 								"&7Tous les cooldowns ont bien été supprimés."),
-		CLEAR_ERROR_STAFF("clearErrorStaff", 				"&6{player} &cn'a aucun cooldown."),
-		CLEAR_ERROR_PLAYER("clearErrorEquals", 				"&cVous n'avez aucun cooldown."),
+		CLEAR_DESCRIPTION(					"Supprime tous les cooldowns d'un joueur"),
+		CLEAR_STAFF(						"&7Tous les cooldowns de &6{player} &7ont bien été supprimés."),
+		CLEAR_PLAYER(						"&7Tous vos cooldowns ont été supprimés par &6{staff}&7."),
+		CLEAR_EQUALS(						"&7Tous vos cooldowns ont bien été supprimés."),
+		CLEAR_ALL(							"&7Tous les cooldowns ont bien été supprimés."),
+		CLEAR_ERROR_STAFF(					"&6{player} &cn'a aucun cooldown."),
+		CLEAR_ERROR_PLAYER(					"&cVous n'avez aucun cooldown."),
 		
 		// List
-		LIST_DESCRIPTION("listDescription", 				"Affiche la liste des cooldowns d'un joueur"),
-		LIST_PLAYER_TITLE("listPlayerTitle", 				"&aLa liste de vos cooldowns"),
-		LIST_PLAYER_LINE("listPlayerLine", 					"    &6&l➤  &6{cooldown} : &a{time} {delete}"),
-		LIST_PLAYER_EMPTY("listPlayerEmpty", 				"&7Vous n'avez aucun cooldown."),
-		LIST_PLAYER_DELETE("listPlayerDelete", 				"&c&nSupprimer"),
-		LIST_PLAYER_DELETE_HOVER("listPlayerDeleteHover", 	"&cCliquez ici pour supprimer le cooldown &6{cooldown}&c."),
-		LIST_STAFF_TITLE("listStaffTitle", 					"&aLa liste des cooldowns de &6<player"),
-		LIST_STAFF_LINE("listStaffLine", 					"    &6&l➤  &6{cooldown} : &a{time} {delete}"),
-		LIST_STAFF_EMPTY("listStaffEmpty", 					"&6{player} &7n'a aucun cooldown."),
-		LIST_STAFF_DELETE("listStaffDelete", 				"&c&nSupprimer"),
-		LIST_STAFF_DELETE_HOVER("listStaffDeleteHover", 	"&cCliquez ici pour supprimer le cooldown &6{cooldown}&c."),
+		LIST_DESCRIPTION(					"Affiche la liste des cooldowns d'un joueur"),
+		LIST_PLAYER_TITLE(					"&aLa liste de vos cooldowns"),
+		LIST_PLAYER_LINE(					"    &6&l➤  &6{cooldown} : &a{time} {delete}"),
+		LIST_PLAYER_EMPTY(					"&7Vous n'avez aucun cooldown."),
+		LIST_PLAYER_DELETE(					"&c&nSupprimer"),
+		LIST_PLAYER_DELETE_HOVER(			"&cCliquez ici pour supprimer le cooldown &6{cooldown}&c."),
+		LIST_STAFF_TITLE(					"&aLa liste des cooldowns de &6<player"),
+		LIST_STAFF_LINE(					"    &6&l➤  &6{cooldown} : &a{time} {delete}"),
+		LIST_STAFF_EMPTY(					"&6{player} &7n'a aucun cooldown."),
+		LIST_STAFF_DELETE(					"&c&nSupprimer"),
+		LIST_STAFF_DELETE_HOVER(			"&cCliquez ici pour supprimer le cooldown &6{cooldown}&c."),
 		
 		// Remove
-		REMOVE_DESCRIPTION("removeDescription", 			"Supprime un cooldown d'un joueur"),
-		REMOVE_STAFF("removeStaff", 						"&7Le cooldown &6{cooldown} &7de &6{player} &7a bien été supprimé."),
-		REMOVE_PLAYER("removePlayer",						"&7Votre cooldown &6{cooldown} &7a été supprimé par &6{staff}&7."),
-		REMOVE_EQUALS("removeEquals", 						"&7Votre cooldown &6{cooldown} &7a bien été supprimé."),
-		REMOVE_ERROR_STAFF("removeErrorStaff", 				"&6{player} &cn'a pas cooldown &6{cooldown}&7."),
-		REMOVE_ERROR_PLAYER("removeErrorEquals", 			"&cVous n'avez pas cooldown &6{cooldown}&7."),
+		REMOVE_DESCRIPTION(					"Supprime un cooldown d'un joueur"),
+		REMOVE_STAFF(						"&7Le cooldown &6{cooldown} &7de &6{player} &7a bien été supprimé."),
+		REMOVE_PLAYER(						"&7Votre cooldown &6{cooldown} &7a été supprimé par &6{staff}&7."),
+		REMOVE_EQUALS(						"&7Votre cooldown &6{cooldown} &7a bien été supprimé."),
+		REMOVE_ERROR_STAFF(					"&6{player} &cn'a pas cooldown &6{cooldown}&7."),
+		REMOVE_ERROR_PLAYER(				"&cVous n'avez pas cooldown &6{cooldown}&7."),
 		
-		PERMISSIONS_COMMANDS_EXECUTE("permissionsCommandsExecute", ""),
-		PERMISSIONS_COMMANDS_HELP("permissionsCommandsHelp", ""),
-		PERMISSIONS_COMMANDS_RELOAD("permissionsCommandsReload", ""),
-		PERMISSIONS_COMMANDS_LIST_EXECUTE("permissionsCommandsListExecute", ""),
-		PERMISSIONS_COMMANDS_LIST_OTHERS("permissionsCommandsListOthers", ""),
-		PERMISSIONS_COMMANDS_CLEAR_EXECUTE("permissionsCommandsClearExecute", ""),
-		PERMISSIONS_COMMANDS_CLEAR_OTHERS("permissionsCommandsClearOthers", ""),
-		PERMISSIONS_COMMANDS_REMOVE_EXECUTE("permissionsCommandsRemoveExecute", ""),
-		PERMISSIONS_COMMANDS_REMOVE_OTHERS("permissionsCommandsRemoveOthers", ""),
-		PERMISSIONS_COOLDOWN("permissionsCooldown", ""),
-		PERMISSIONS_BYPASS("permissionsBypass", "");
+		PERMISSIONS_COMMANDS_EXECUTE(""),
+		PERMISSIONS_COMMANDS_HELP(""),
+		PERMISSIONS_COMMANDS_RELOAD(""),
+		PERMISSIONS_COMMANDS_LIST_EXECUTE(""),
+		PERMISSIONS_COMMANDS_LIST_OTHERS(""),
+		PERMISSIONS_COMMANDS_CLEAR_EXECUTE(""),
+		PERMISSIONS_COMMANDS_CLEAR_OTHERS(""),
+		PERMISSIONS_COMMANDS_REMOVE_EXECUTE(""),
+		PERMISSIONS_COMMANDS_REMOVE_OTHERS(""),
+		PERMISSIONS_COOLDOWN(""),
+		PERMISSIONS_BYPASS("");
 		
 		private final String path;
 	    private final EMessageBuilder french;
@@ -83,24 +83,23 @@ public class ECMessage extends EMessage<EverCooldowns> {
 	    private EMessageFormat message;
 	    private EMessageBuilder builder;
 	    
-	    private ECMessages(final String path, final String french) {   	
-	    	this(path, EMessageFormat.builder().chat(new EFormatString(french), true));
+	    private ECMessages(final String french) {   	
+	    	this(EMessageFormat.builder().chat(new EFormatString(french), true));
 	    }
 	    
-	    private ECMessages(final String path, final String french, final String english) {   	
-	    	this(path, 
-	    		EMessageFormat.builder().chat(new EFormatString(french), true), 
+	    private ECMessages(final String french, final String english) {   	
+	    	this(EMessageFormat.builder().chat(new EFormatString(french), true), 
 	    		EMessageFormat.builder().chat(new EFormatString(english), true));
 	    }
 	    
-	    private ECMessages(final String path, final EMessageBuilder french) {   	
-	    	this(path, french, french);
+	    private ECMessages(final EMessageBuilder french) {   	
+	    	this(french, french);
 	    }
 	    
-	    private ECMessages(final String path, final EMessageBuilder french, final EMessageBuilder english) {
+	    private ECMessages(final EMessageBuilder french, final EMessageBuilder english) {
 	    	Preconditions.checkNotNull(french, "Le message '" + this.name() + "' n'est pas définit");
 	    	
-	    	this.path = path;	    	
+	    	this.path = this.resolvePath();	    	
 	    	this.french = french;
 	    	this.english = english;
 	    	this.message = french.build();
